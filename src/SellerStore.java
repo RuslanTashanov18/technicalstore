84bc78df0ab8b1835212fbece0d253fe1687166e
RuslanTashanov18 <tashanov.2019@gmail.com>
2025-02-26 00:27:05 +0500

    Начальная реализация Магазина техники

---
+++ src/SellerStore.java
@@ -0,0 +1,13 @@
+public class SellerStore {
+    public static void main(String[] args) {
+        greetCustomer();
+        offerCustomer();
+    }
+    public static void greetCustomer(){
+        System.out.println("Welcome to our technical store!");
+    }
+    public static void offerCustomer(){
+        System.out.println("Do you want to buy something?");
+        System.out.println("We have devices, headphones and laptops by Apple, Sumsung, Oppo");
+    }
+}
